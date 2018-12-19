ea0f8f004a42eef2eee452c896dade02bc8cfc93
JasperHackett <jasper.hackett@live.com.au>
2018-12-19 19:39:59 +1100

    fix

--- Project Resources/src/Game.java
+++ Project Resources/src/Game.java
@@ -12,19 +12,16 @@ public class Game {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-		GameWindow mainGameWindow = new GameWindow();
-		Renderer mainGameRenderer = new Renderer("mainGameRenderer",mainGameWindow);
-		mainGameRenderer.start();
-
 		// TODO Auto-generated method stub
 		System.out.println("Test");
-
-
-
+		
+		Renderer mainGameRenderer = new Renderer("mainGameRenderer");
+		mainGameRenderer.start();
+		GameWindow test = new GameWindow();
 		
 
-	}		
+		
+	}
 	
 	
 	
